e9143dcaf57273f601f0db1398d10472f07d6d50
glg2013 <57412960@qq.com>
2022-03-21 05:38:11 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,4 +68,61 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderByDesc('created_at');
     }
+
+
+    /**
+     * 查找粉丝
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     * 查找关注的人
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 关注
+     * @param $user_ids
+     * @return void
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    /**
+     * 取关
+     * @param $user_ids
+     * @return void
+     */
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 是否是关注的人
+     * @param $user_id
+     * @return bool
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->allRelatedIds()->contains($user_id);
+    }
+
 }
